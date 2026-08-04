e49280aaa01f1ac0e3ebc7b7011ffa1f59324255
JasonDavD <jason.dav.del@gmail.com>
2026-08-03 22:53:01 -0500

    refactor: impedir la instanciación de las clases de ui
    LoginPage ya tenía constructor privado; InventoryPage no declaraba
    ninguno y ProductDetailPage lo tenía público. Estas clases solo
    contienen constantes de selectores, por lo que instanciarlas no
    debería compilar.

--- src/test/java/edu/pe/cibertec/saucedemo/ui/ProductDetailPage.java
+++ src/test/java/edu/pe/cibertec/saucedemo/ui/ProductDetailPage.java
@@ -6,5 +6,5 @@ public class ProductDetailPage {
     public static final String PRODUCT_PRICE = "[data-test='inventory-item-price']";
     public static final String ADD_TO_CART_BUTTON = "#add-to-cart";
 
-    public ProductDetailPage() {}
+    private ProductDetailPage() {}
 }
